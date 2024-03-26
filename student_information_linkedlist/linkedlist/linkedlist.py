04a08ed921b08821ca757732fa952430b06d3115
suzhenjiang <suzhenjiang1031@gmail.com>
2024-03-26 23:49:43 +0800

    学生信息链表

---
+++ student_information_linkedlist/linkedlist/linkedlist.py
@@ -0,0 +1,37 @@
+class Student:
+    def __init__(self, num, name, score):
+        self.num = num
+        self.name = name
+        self.score = score
+        self.next = None
+
+def build_linked_list():
+    head = None
+    tail = None
+
+    while True:
+        line = input()
+        if line == '#':
+            break
+        num, name, score = line.split()
+        student = Student(num, name, score)
+
+        if head is None:
+            head = student
+            tail = student
+        else:
+            tail.next = student
+            tail = student
+    return head
+
+def print_linked_list(head):
+    current = head
+    while current is not None:
+        print(current.num, current.name, current.score, end=",")
+        current = current.next
+        
+while True:
+    head = build_linked_list()
+    if head is None:
+        break
+    print_linked_list(head)
