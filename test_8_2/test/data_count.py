a3d5ad4ac75de9a4e874b1ab00f17fc7f2994414
suzhenjiang1031 <suzhenjiang1031@gmail.com>
2024-11-20 22:08:05 +0800

    test

---
+++ test_8_2/test/data_count.py
@@ -0,0 +1,8 @@
+class BinaryTreeNode(object):
+    def __init__(self):
+        self.data = '#'
+        self.lchild = None
+        self.rchild = None
+
+class TreeState(object):
+    def __init__(self, Binary):
