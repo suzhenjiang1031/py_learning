b30db3130d13f929b9772ce8bcdfe75f14806482
suzhenjiang1031 <suzhenjiang1031@gmail.com>
2025-01-17 10:21:16 +0800

    counting_sort

---
+++ sorting/counting_sort.py
@@ -0,0 +1,16 @@
+def counting_sort(arr):
+    max_val = max(arr)
+    min_val = min(arr)
+    range_of_elements = max_val - min_val + 1
+    count = [0] * range_of_elements
+    output = [0] * len(arr)
+    for num in arr:
+        count[num - min_val] += 1
+    for i in range(1, len(count)):
+        count[i] += count[i - 1]
+    for num in reversed(arr):
+        output[count[num - min_val] - 1] = num
+        count[num - min_val] -= 1
+    return output
+
+print(counting_sort([64, 34, 25, 12, 22, 11, 90]))
