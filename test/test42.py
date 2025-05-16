63eba6f49ef277bd5c324e03b76435a85de7c672
suzhenjiang1031 <suzhenjiang1031@gmail.com>
2025-05-16 23:43:33 +0800

    test

---
+++ test/test42.py
@@ -0,0 +1,12 @@
+def knapsack_dp(weights, values, capacity):
+    n = len(weights)
+    dp = [[0]*(capacity+1) for _ in range(n+1)]
+    for i in range(1, n+1):
+        for w in range(capacity+1):
+            if weights[i-1] <= w:
+                dp[i][w] = max(dp[i-1][w], dp[i-1][w - weights[i-1]] + values[i-1])
+            else:
+                dp[i][w] = dp[i-1][w]
+    return dp[n][capacity]
+
+print("最大价值：", knapsack_dp(weights, values, capacity))
